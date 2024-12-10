bebe52b8814713c73a1520d261ab6cd8bce26d51
Nicola Kirk <nicola.kirk@autotrader.co.uk>
2024-12-10 15:13:33 +0000

    started making app

---
+++ src/app/Home-component/Home-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Home-component',
+  templateUrl: './Home-component.component.html',
+  styleUrls: ['./Home-component.component.css']
+})
+export class HomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
