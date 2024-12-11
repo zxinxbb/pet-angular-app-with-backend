8490c759f99d8124355218520d709d3c4df696a4
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2024-12-11 19:26:43 +0000

    add template url and style url to component

--- src/app/pet-post/pet-post.component.ts
+++ src/app/pet-post/pet-post.component.ts
@@ -1,15 +1,18 @@
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule} from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 // import { SlackService } from './slack.service';
 
 @Component({
   selector: 'app-pet-post',
-  template: `
-  <p> this works</p>
-    <!-- <button (click)="postPetToSlack()">Post Pet to Slack</button> -->
-  `
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl:'./pet-post.component.html',
+  styleUrls: ['./pet-post.component.css'],
 })
 export class PetPostComponent {
+onImageChange: any;
   constructor () {}
   form = new FormGroup({
     userName: new FormControl('', Validators.required),
@@ -19,5 +22,13 @@ export class PetPostComponent {
     type: new FormControl('', Validators.required),
     image: new FormControl('', Validators.required),
 }
-  )
+  );
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted:', this.form.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
+
