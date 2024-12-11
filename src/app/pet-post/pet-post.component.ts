b2a6ee789b80af1772d56b68cc541e11e053e43d
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2024-12-11 15:22:06 +0000

    update forms on pet-post

--- src/app/pet-post/pet-post.component.ts
+++ src/app/pet-post/pet-post.component.ts
@@ -1,4 +1,4 @@
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 // import { SlackService } from './slack.service';
 
@@ -11,12 +11,13 @@ import { Component } from '@angular/core';
 })
 export class PetPostComponent {
   constructor () {}
-  // constructor(private slackService: SlackService) {}
-
-  // postPetToSlack() {
-  //   const message = 'Meet Fluffy, my adorable cat looking for a weekend buddy! 🐱';
-  //   this.slackService.sendMessageToSlack(message).subscribe(response => {
-  //     console.log('Message posted to Slack:', response);
-  //   });
-  }
-
+  form = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    petName: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    age: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+}
+  )
+}
